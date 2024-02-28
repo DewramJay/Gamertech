c6e96173eece4e6b179555ffd23d7ab4086e0b02
DewramJay <jayasinghe_jaadk_e21@engug.ruh.ac.lk>
2024-02-28 21:35:22 +0530

    filter changed

--- src/views/softmatter/table_search_form_softmatter.tsx
+++ src/views/softmatter/table_search_form_softmatter.tsx
@@ -16,6 +16,10 @@ interface MyComponentProps {
 const MyComponent: React.FC<MyComponentProps> = ({ initialValues = {}, onInputChange }) => {
   const [fieldValues, setFieldValues] = useState<Record<string, string>>(initialValues);
 
+  const optionsField1 = ["00", "option2"];
+  const optionsField2 = ["54", "option2"];
+  const optionsPassFail = ["Pass", "Fail"];
+
   const handleInputChangeLocal = (fieldName: string) => (event: ChangeEvent<HTMLInputElement | { value: string }>) => {
     const newValue = event.target.value;
     setFieldValues((prevValues) => ({
@@ -47,54 +51,63 @@ const MyComponent: React.FC<MyComponentProps> = ({ initialValues = {}, onInputCh
 
   return (
     <div style={{ display: 'flex', flexDirection: 'row', gap: '8px' }}>
-  <FormControl fullWidth>
-    <InputLabel id="demo-simple-select-label">MAC Address</InputLabel>
-    <Select
-      labelId="demo-simple-select-label"
-      id="demo-simple-select"
-      label="Field 1"
-      variant="outlined"
-      value={fieldValues.field1 || ''}
-      onChange={handleSelectChange('field1')}
-      style={selectFieldStyle}
-    >
-      <MenuItem value="00">00</MenuItem>
-      <MenuItem value="option2">Option 2</MenuItem>
-    </Select>
-  </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">MAC Address</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Field 1"
+          variant="outlined"
+          value={fieldValues.field1 || ''}
+          onChange={handleSelectChange('field1')}
+          style={selectFieldStyle}
+        >
+          {optionsField1.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
 
-  <FormControl fullWidth>
-    <InputLabel id="demo-simple-select-label">Production Order</InputLabel>
-    <Select
-      labelId="demo-simple-select-label"
-      id="demo-simple-select"
-      label="Field 2"
-      variant="outlined"
-      value={fieldValues.field2 || ''}
-      onChange={handleSelectChange('field2')}
-      style={selectFieldStyle}
-    >
-      <MenuItem value="54">54</MenuItem>
-      <MenuItem value="option2">Option 2</MenuItem>
-    </Select>
-  </FormControl>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Production Order</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Field 2"
+          variant="outlined"
+          value={fieldValues.field2 || ''}
+          onChange={handleSelectChange('field2')}
+          style={selectFieldStyle}
+        >
+          {optionsField2.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
 
-  <FormControl fullWidth>
-    <InputLabel id="demo-simple-select-label">Result</InputLabel>
-    <Select
-      labelId="demo-simple-select-label"
-      id="demo-simple-select"
-      label="Field 3"
-      variant="outlined"
-      value={fieldValues.field3 || ''}
-      onChange={handleSelectChange('field3')}
-      style={selectFieldStyle}
-    >
-      <MenuItem value="Pass">Pass</MenuItem>
-      <MenuItem value="Fail">Fail</MenuItem>
-    </Select>
-  </FormControl>
-</div>
+      <FormControl fullWidth>
+        <InputLabel id="demo-simple-select-label">Result</InputLabel>
+        <Select
+          labelId="demo-simple-select-label"
+          id="demo-simple-select"
+          label="Field 3"
+          variant="outlined"
+          value={fieldValues.field3 || ''}
+          onChange={handleSelectChange('field3')}
+          style={selectFieldStyle}
+        >
+          {optionsPassFail.map((option) => (
+            <MenuItem key={option} value={option}>
+              {option}
+            </MenuItem>
+          ))}
+        </Select>
+      </FormControl>
+    </div>
   );
 };
 
